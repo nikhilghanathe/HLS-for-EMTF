d3a4acf3367a16abec51d30616b6a44da28f38eb
Nikhil Pratap Ghanathe <nikhilghanathe@gmail.com>
2017-05-08 16:31:09 -0400

    Add files via upload

--- sources/extender.cpp
+++ sources/extender.cpp
@@ -1,52 +1,27 @@
 #include <ap_int.h>
 #include "spbits.h"
 #include "extender.h"
-
-
-
 void extend::extender(   ap_uint<ph_raw_w> inp,
 			  ap_uint<ph_raw_w>	*outp,
-			ap_uint<3> drifttime)
-			{
+			  ap_uint<3> drifttime){
+
 #pragma HLS INTERFACE ap_stable port=inp  //This directive optimizes unnecessary registers
-#pragma HLS INTERFACE ap_none port=outp
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS INLINE off
 #pragma HLS PIPELINE II=1
 
-		//build delay lines for each bit
-		     volatile ap_uint<ph_raw_w> d[2];
-	      ap_uint<ph_raw_w> t1, t2;
+volatile ap_uint<ph_raw_w> d[2];
+ ap_uint<ph_raw_w> temp;
+
+//d[1]=temp2;
+temp=inp;
+temp= ap_uint<ph_raw_w>(temp) | ap_uint<ph_raw_w>(temp1) | temp2;
+//d[0]=temp1;
+//d[1]=d[0];
+//d[0]=temp1;
+temp2=temp1;
+temp1=inp;
+*outp=temp;
 
 
-		     //volatile temp variable to preserve the operation and prevent HLS optimization
-		      volatile  ap_uint<ph_raw_w>temp;
-
-
-		      //load previous two bunch crossings
-		      	 t1=bx1;
-		      	 t2=bx2;
-
-		     //use volatile variable to prevent HLS optimization
-		      temp = inp | bx1 | bx2;
-
-
-		      //Build delay line
-		      	d[0]=t1;
-		      	d[1]=t2;
-		      	d[1]=d[0];
-		      	d[0]=inp;
-
-		      	//store current and previous bunch crossing values for use in next function call
-				bx1=d[0];
-				bx2=d[1];
-
-				//assign output
-				*outp=temp;
-
-
-
-
-
-
-			}
+}
